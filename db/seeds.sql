1f4d821e9085ebf466d9e75d620c3066d8508f92
Daniel Groveman <daniel5075@gmail.com>
2019-12-11 20:31:57 -0600

    updated folder to pattern MVC, database is working

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burger (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burger (burger_name) VALUES ('Bacon Swiss Burger');
+INSERT INTO burger (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burger (burger_name) VALUES ('Blue Cheese Burger with Onions');
